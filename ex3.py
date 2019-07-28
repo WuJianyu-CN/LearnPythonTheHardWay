77b909179fdd380f035fa0d96b7406827a2f8758
jaywu201218 <jaywu201218@hotmail.com>
2019-07-28 10:52:05 +0800

    add 4 python files

---
+++ ex3.py
@@ -0,0 +1,24 @@
+# print the notice
+print("I will now count my chickens:")
+# count the numbers of hens and rossters
+print("Hens", 25 + 30 / 6)
+print("Rossters", 100 - 25 * 3 % 4)
+# print the notice
+print("Now I will count the eggs:")
+# count the numbers of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# judge whether 3 + 2< 5-7 is true and print the result
+print("Is it true that 3 + 2 < 5 -7?")
+# print the result
+print(3 + 2 < 5 - 7)
+# calculate some expression and find the result
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+# print
+print("Oh, that's why it's False.")
+# print
+print("How about some more.")
+# judge some expression
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
